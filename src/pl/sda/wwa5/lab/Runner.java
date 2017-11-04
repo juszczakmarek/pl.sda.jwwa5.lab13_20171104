07057df9cf27551f720aee41223609639a0739d7
Marek Juszczak <juszczak.marek@gmail.com>
2017-11-04 14:30:01 +0100

    few methods

--- src/pl/sda/wwa5/lab/Runner.java
+++ src/pl/sda/wwa5/lab/Runner.java
@@ -1,5 +1,8 @@
 package pl.sda.wwa5.lab;
 
+import java.io.FileNotFoundException;
+import java.util.List;
+
 /**
  * Details described in @see pl/sda/wwa5/lab/applicationModel
  */
@@ -7,6 +10,23 @@ package pl.sda.wwa5.lab;
 public class Runner {
 
     public static void main(String[] args) {
-	// write your code here
+
+        if(args.length ==0) {
+            System.out.println("Podaj poprawne parametry");
+            System.exit(1);
+        }
+
+        WarehouseDao warehouseDao = new WarehouseDao(new ProductConverter(),"warehouse.csv");
+        Warehouse warehouse = new Warehouse(warehouseDao);
+        Shop shop = new Shop(warehouse);
+
+        if(args[0].equals("pokazprodukty")) {
+            try {
+                List<Product> products = shop.showProducts();
+            } catch (FileNotFoundException e) {
+                System.out.println("Nie znaleziono pliku warehouse.csv");
+            }
+        }
+
     }
 }
